ef453425e6cf102901ad8e32f817d0393ed77961
tayqiter00 <tayqiter00@gmail.com>
2025-01-13 13:05:34 +0800

    Add includes

---
+++ includes/Webserv.hpp
@@ -0,0 +1,28 @@
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <set>
+#include <iomanip>
+#include <arpa/inet.h>
+#include <cstdint>
+#include <limits>
+#include <algorithm>
+#include <fstream>
+
+#define BLACK "\001\033[30m\002"
+#define RED "\001\033[31m\002"
+#define GREEN "\001\033[32m\002"
+#define YELLOW "\001\033[33m\002"
+#define BLUE "\001\033[34m\002"
+#define MAGENTA "\001\033[35m\002"
+#define CYAN "\001\033[36m\002"
+#define WHITE "\001\033[37m\002"
+#define GRAY "\001\033[90m\002"
+#define LIGHT_GRAY "\001\033[37m\002"
+#define BEIGE "\001\033[93m\002"
+#define RESET "\001\033[0m\002"
+
+#endif
